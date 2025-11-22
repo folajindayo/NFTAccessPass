83b196991b3b79ce59807455195268276db4021f
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:30:57 +0100

    Theme: Define typography tokens

---
+++ src/theme/typography.ts
@@ -0,0 +1,12 @@
+export const typography = {
+  h1: 'text-4xl font-bold',
+  h2: 'text-2xl font-bold',
+  h3: 'text-xl',
+  body: 'text-base',
+  small: 'text-sm',
+  weight: {
+    bold: 'font-bold',
+    semibold: 'font-semibold'
+  }
+};
+
