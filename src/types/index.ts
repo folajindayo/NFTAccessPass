3baa10c3902e5dac31499047adc44e7a5c822367
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:36:42 +0100

    Refactor: Export types from barrel file

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,20 +1,2 @@
-export interface MintResponse {
-  success: boolean;
-  txHash?: string;
-  error?: string;
-}
-
-export interface CheckResponse {
-  hasAccess: boolean;
-  balance?: string;
-  error?: string;
-}
-
-export interface AccessCardProps {
-  isConnected: boolean;
-  hasAccess: boolean;
-  loading: boolean;
-  message: string;
-  onMint: () => void;
-}
-
+export * from './api';
+export * from './components';
