2e9a03e4bf5b124f8f589c2c27666bf436ba7911
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:10:58 +0100

    Fix: Remove unnecessary ts-expect-error

--- src/components/WalletConnect.tsx
+++ src/components/WalletConnect.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 export const WalletConnect = () => {
   return (
     <div className="mb-8">
-      {/* @ts-expect-error - web component */}
       <appkit-button />
     </div>
   );
