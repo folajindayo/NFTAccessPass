1d7295e6bce7c4d1bf1894e77672a9c6c68d25ec
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 17:44:56 +0100

    Update contracts/NFTAccessPass.sol - 1763829896

--- contracts/NFTAccessPass.sol
+++ contracts/NFTAccessPass.sol
@@ -2,10 +2,6 @@
 pragma solidity ^0.8.27;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
-
-// Note: Counters is deprecated in newer OZ versions, I'll check if I can use it or just a simple uint256
-// OpenZeppelin 5.x removed Counters. I'll use a simple uint256 private _nextTokenId;
 
 contract NFTAccessPass is ERC721 {
     uint256 private _nextTokenId;
