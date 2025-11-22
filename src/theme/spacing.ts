11a0df08ad4edb747781ea510d714a127b4880cd
folajindayo <temidayofolajin@yahoo.com>
2025-11-22 18:30:43 +0100

    Theme: Define spacing tokens

---
+++ src/theme/spacing.ts
@@ -0,0 +1,14 @@
+export const spacing = {
+  container: 'p-24',
+  card: 'p-6',
+  input: 'px-6 py-2',
+  gap: {
+    small: 'gap-4',
+    medium: 'gap-8'
+  },
+  margin: {
+    bottom: 'mb-8',
+    top: 'mt-4'
+  }
+};
+
