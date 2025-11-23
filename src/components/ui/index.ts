3c563755b379c7a8131c70193b72b5979c46b307
folajindayo <temidayofolajin@yahoo.com>
2025-11-23 17:30:21 +0100

    Refactor: Export new layout components from ui index

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -2,4 +2,7 @@ export * from './Button';
 export * from './Card';
 export * from './Text';
 export * from './Heading';
-
+export * from './Box';
+export * from './Flex';
+export * from './Grid';
+export * from './Divider';
