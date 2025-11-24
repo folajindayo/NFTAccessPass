e4329b6e418f6fc9effab9333ab13c78e66eaa43
folajindayo <temidayofolajin@yahoo.com>
2025-11-24 20:41:29 +0100

    Feat: Add UserIcon

---
+++ src/components/icons/UserIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+}
+
+export const UserIcon: React.FC<IconProps> = ({ size = 24, className = '', ...props }) => (
+  <svg 
+    width={size} 
+    height={size} 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round"
+    className={className}
+    {...props}
+  >
+    <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2" />
+    <circle cx="12" cy="7" r="4" />
+  </svg>
+);
+
