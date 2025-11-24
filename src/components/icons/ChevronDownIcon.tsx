0ff470156de4511669ce1987f3317f86905a1982
folajindayo <temidayofolajin@yahoo.com>
2025-11-24 13:01:40 +0100

    Feat: Add ChevronDownIcon

---
+++ src/components/icons/ChevronDownIcon.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const ChevronDownIcon = ({ className = "w-6 h-6" }) => (
+  <svg className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+  </svg>
+);
+
