3216925e96f84348e777e7012c8b089c1c68e6e1
folajindayo <temidayofolajin@yahoo.com>
2025-11-24 20:39:21 +0100

    Feat: Add ArrowLeftIcon

---
+++ src/components/icons/ArrowLeftIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+interface IconProps extends React.SVGProps<SVGSVGElement> {
+  size?: number;
+}
+
+export const ArrowLeftIcon: React.FC<IconProps> = ({ size = 24, className = '', ...props }) => (
+  <svg 
+    width={size} 
+    height={size} 
+    viewBox="0 0 24 24" 
+    fill="none" 
+    stroke="currentColor" 
+    strokeWidth="2" 
+    strokeLinecap="round" 
+    strokeLinejoin="round"
+    className={className}
+    {...props}
+  >
+    <line x1="19" y1="12" x2="5" y2="12" />
+    <polyline points="12 19 5 12 12 5" />
+  </svg>
+);
+
