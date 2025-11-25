f954c44c9296344bc000a8ceae6eeae9c76e4b09
folajindayo <temidayofolajin@yahoo.com>
2025-11-25 22:50:02 +0100

    refactor(auto): standardize imports in XIcon.test.tsx

--- src/__tests__/components/icons/XIcon.test.tsx
+++ src/__tests__/components/icons/XIcon.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+
 import { XIcon } from '@/components/icons/XIcon';
+import { render } from '@testing-library/react';
 
 describe('XIcon', () => {
   it('renders correctly', () => {
