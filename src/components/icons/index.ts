a5342b445bfd6a2c6fe8390c32e071758dfcbf6b
folajindayo <temidayofolajin@yahoo.com>
2025-11-28 15:15:45 +0100

    Update icons index with all new icon exports

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,13 +1,37 @@
-export * from './CheckIcon';
-export * from './XIcon';
-export * from './MenuIcon';
-export * from './ChevronDownIcon';
-export * from './ChevronUpIcon';
-export * from './ArrowRightIcon';
-export * from './ArrowLeftIcon';
-export * from './SearchIcon';
-export * from './SettingsIcon';
-export * from './UserIcon';
-export * from './WalletIcon';
-export * from './LoadingIcon';
-export * from './AlertIcon';
+// Copy icons
+export { CopyIcon, CopyCheckIcon } from './CopyIcon';
+
+// External link icons
+export { ExternalLinkIcon, ExternalLinkSmallIcon } from './ExternalLinkIcon';
+
+// Refresh icons
+export { RefreshIcon, RefreshSpinningIcon } from './RefreshIcon';
+
+// Shield icons
+export { ShieldIcon, ShieldCheckIcon, ShieldAlertIcon } from './ShieldIcon';
+
+// Lock icons
+export { LockIcon, LockClosedIcon } from './LockIcon';
+
+// Unlock icons
+export { UnlockIcon, UnlockOpenIcon } from './UnlockIcon';
+
+// Info icons
+export { InfoIcon, InfoCircleIcon } from './InfoIcon';
+
+// Warning icons
+export { WarningIcon, WarningTriangleIcon } from './WarningIcon';
+
+// Success icons
+export { SuccessIcon, SuccessCircleIcon, CheckIcon } from './SuccessIcon';
+
+// Re-export default icons for convenience
+export { default as CopyIconDefault } from './CopyIcon';
+export { default as ExternalLinkIconDefault } from './ExternalLinkIcon';
+export { default as RefreshIconDefault } from './RefreshIcon';
+export { default as ShieldIconDefault } from './ShieldIcon';
+export { default as LockIconDefault } from './LockIcon';
+export { default as UnlockIconDefault } from './UnlockIcon';
+export { default as InfoIconDefault } from './InfoIcon';
+export { default as WarningIconDefault } from './WarningIcon';
+export { default as SuccessIconDefault } from './SuccessIcon';
